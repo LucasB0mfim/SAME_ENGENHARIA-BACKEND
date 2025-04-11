a464fa2b3f4d36063ee9050603c74a5d40cab1bc
LucasB0mfim <lucasbomfimsobral@gmail.com>
2025-04-11 14:00:40 -0300

    Fix: Corrigir método para buscar todos os registros de ratreamento

--- src/repositories/tracking.repository.js
+++ src/repositories/tracking.repository.js
@@ -6,66 +6,63 @@ import logger from '../utils/logger/winston.js';
 class TrackingRepository {
     async findAll() {
         try {
-            logger.info('Solicitação para retornar a tabela de tracking.');
+            await pool.connect();
+            const request = pool.request();
+            const result = await request.query('exec tracking');
 
-            // if (!pool.connected) {
-            //     await pool.connect();
-            // }
+            if (!result.recordset || result.recordset.length === 0) {
+                logger.error('Erro ao retornar a tabela de tracking');
+                throw new AppError('Erro ao retornar a tabela de tracking', 404);
+            }
 
-            // const request = pool.request();
-            // const result = await request.query('exec tracking');
+            const updatetrk = Array.from(
+                new Map(
+                    result.recordset.map(data => [
+                        data.id,
+                        {
+                            cotacao: data.cotacao,
+                            integracao: data.integracao,
+                            centro_custo: data.centro_custo,
+                            movimento: data.movimento,
+                            id: data.id,
+                            elaboracao_pedido: data.elaboracao_pedido,
+                            aprovacao_id: data.aprovacao_id,
+                            numero_oc: data.numero_oc,
+                            elaboracao_oc: data.elaboracao_oc,
+                            aprovacao_oc: data.aprovacao_oc,
+                            data_entrega: data.data_entrega,
+                            fornecedor: data.fornecedor,
+                            material: data.material,
+                            quantidade_total: data.quantidade_total,
+                            unidade: data.unidade,
+                            status: data.status,
+                            criacao: data.criacao
+                        }
+                    ])
+                ).values()
+            );
 
-            // if (!result.recordset || result.recordset.length === 0) {
-            //     logger.error('Erro ao retornar a tabela de tracking');
-            //     throw new AppError('Erro ao retornar a tabela de tracking', 404);
-            // }
+            // ATUALIZA A TABELA
+            const { error: upsertError } = await dataBase
+                .from('tracking')
+                .upsert(updatetrk, { onConflict: 'id' });
 
-            // logger.info('Tabela de tracking encontrada.');
+            if (upsertError) {
+                logger.error('Não foi possível atualizar os dados no Supabase', { error: upsertError });
+                throw new AppError('Não foi possível atualizar os dados no Supabase', 404);
+            }
 
-            // const updatetrk = Array.from(
-            //     new Map(
-            //         result.recordset.map(data => [
-            //             data.id, // Usa 'id' como chave única
-            //             {
-            //                 cotacao: data.cotacao,
-            //                 integracao: data.integracao,
-            //                 centro_custo: data.centro_custo,
-            //                 movimento: data.movimento,
-            //                 id: data.id,
-            //                 elaboracao_pedido: data.elaboracao_pedido,
-            //                 aprovacao_id: data.aprovacao_id,
-            //                 numero_oc: data.numero_oc,
-            //                 elaboracao_oc: data.elaboracao_oc,
-            //                 aprovacao_oc: data.aprovacao_oc,
-            //                 data_entrega: data.data_entrega,
-            //                 fornecedor: data.fornecedor,
-            //                 material: data.material,
-            //                 quantidade_total: data.quantidade_total,
-            //                 unidade: data.unidade,
-            //                 status: data.status,
-            //                 criacao: data.criacao
-            //             }
-            //         ])
-            //     ).values()
-            // );
-
-            // logger.info('Atualizando banco de dados auxiliar.');
-
-            // const { data, error } = await dataBase
-            //     .from('tracking')
-            //     .upsert(updatetrk, { onConflict: 'id' })
-            //     .select('*');
-
+            // BUSCA OS REGISTROS
             const { data, error } = await dataBase
                 .from('tracking')
                 .select('*');
 
             if (!data || error) {
-                logger.error('Não foi possível transferir os dados para o Supabase', { error });
-                throw new AppError(`Não foi possível transferir os dados para o Supabase: ${error.message}`);
+                logger.error('Não foi possível buscar os dados do Supabase', { error });
+                throw new AppError(`Não foi possível buscar os dados do Supabase: ${error?.message}`);
             }
 
-            logger.info('Dados transferidos com sucesso.');
+            logger.info(`${data.length} registros recuperados do Supabase.`);
             return data;
         } catch (error) {
             console.error('Erro ao buscar todos os registros:', error);
