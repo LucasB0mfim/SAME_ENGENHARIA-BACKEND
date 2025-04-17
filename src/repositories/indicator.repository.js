9373ac46203859c5c7463ba58cf128357395b54e
lucas <lucasbomfimsobral@gmail.com>
2025-04-17 10:58:15 -0300

    feat: Adicionar método para transferir dados para a tabela.

--- src/repositories/indicator.repository.js
+++ src/repositories/indicator.repository.js
@@ -5,9 +5,50 @@ import logger from '../utils/logger/winston.js';
 
 class IndicatorRepository {
     async findCostCenter() {
-        await pool.connect();
-        const result = await pool.request().query('exec financial');
-        return result;
+        try {
+            await pool.connect();
+
+            const result = await pool.request().query('exec financial');
+
+            const costCenter = result.recordset.map(item => ({
+                nome_centro_custo: item.nome_centro_custo,
+                total_receber: item.total_receber,
+                total_pago_material: item.total_pago_material,
+                material_pago: item.material_pago,
+                material_apagar: item.material_apagar,
+                total_pago_servico: item.total_pago_servico,
+                servico_pago: item.servico_pago,
+                servico_apagar: item.servico_apagar,
+                folha_pagamento: item.folha_pagamento
+            }));
+
+            // ATUALIZA A TABELA
+            const { error: upsertError } = await dataBase
+                .from('financial')
+                .upsert(costCenter);
+
+            if (upsertError) {
+                logger.error('Não foi possível atualizar a tabela: ', { error: upsertError });
+                throw new AppError('Não foi possível atualizar a tabela: ', 404);
+            }
+
+            // BUSCA OS REGISTROS
+            const { data, error } = await dataBase
+                .from('financial')
+                .select('*');
+
+            if (!data || error) {
+                logger.error('Não foi possível buscar os dados da tabela: ', { error });
+                throw new AppError('Não foi possível buscar os dados da tabela: ', 404);
+            }
+
+            logger.info(`${data.length} registros encontrados.`);
+
+            return data;
+        } catch (error) {
+            logger.error('Erro ao sincronizar pedidos.', { error });
+            throw error;
+        }
     }
 }
 
