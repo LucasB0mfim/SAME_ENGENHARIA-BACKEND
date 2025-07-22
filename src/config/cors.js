52fa5da4a566e2ec97c86ec90890ddc7e93d26b8
lucas <lucasbomfimsobral@gmail.com>
2025-07-22 09:49:03 -0300

    Fix: Liberar acesso ao cors

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,6 +1,8 @@
 const config = {
-    origin: '*',
-    methods: ['POST', 'GET', 'PUT', 'DELETE']
+    origin: ['https://sameengenharia.com.br', 'https://www.sameengenharia.com.br'],
+    methods: ['POST', 'GET', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
 };
 
 export default config;
